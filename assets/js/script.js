6d52065c83f99d17975369449aac28d7a40df1af
Dea Ananda Gunawan <Deaanandagunawan@gmail.com>
2022-07-11 18:32:01 +0700

    Portfolio first attempt: f/home

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const guest = {
+  name: guestName,
+  language: language,
+};
+
+function updateDisplay() {
+  // Tangkap elemen yang dituju
+    const displayName = document.getElementById("displayName");
+    displayName.innerHTML = ('')
+}
+
+if (guest.language === "Indonesia" || "indonesia") {
+}
